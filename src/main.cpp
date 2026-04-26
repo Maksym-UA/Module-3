a67a63319fccb9e0becc733ccf6defb5a883fa4e
Maksym-UA <max.savin3@gmail.com>
2026-04-26 12:19:11 +0300

    Refactor after review

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ struct Config {
   static constexpr uint8_t ADC_PIN = 4; //ADC1_CH3
   static constexpr uint32_t BAUDRATE = 115200;
   static constexpr float VCC = 3.3; //volts
-  static constexpr int ADC_RES = 4095; // 12-bit resolution (0-4095)
+  static constexpr int ADC_RES_BITS = 12; // 12-bit resolution
+  static constexpr int ADC_MAX = (1 << ADC_RES_BITS) - 1;
   static esp_adc_cal_characteristics_t adc_chars;
 };
 
@@ -15,7 +16,7 @@ esp_adc_cal_characteristics_t Config::adc_chars;
 void setup() {
 
   Serial.begin(Config::BAUDRATE);
-  analogReadResolution(12); // Set ADC resolution to 12 bits
+  analogReadResolution(Config::ADC_RES_BITS); // Set ADC resolution to 12 bits
   analogSetAttenuation(ADC_11db);// Set attenuation to 11dB for a wider voltage range (up to ~3.6V)
 
   // Characterize ADC
@@ -27,7 +28,7 @@ void loop() {
   int rawValue = analogRead(Config::ADC_PIN);
 
   // Convert to mV using the formula: (raw / max_raw) * VCC * 1000
-  float voltageCalc = (rawValue / (float)Config::ADC_RES) * Config::VCC * 1000.0; // Convert to mV
+  float voltageCalc = (rawValue / (float)Config::ADC_MAX) * Config::VCC * 1000.0; // Convert to mV
 
   // Get calibrated voltage in mV using the ESP32 ADC calibration function
   uint32_t voltageCalib = esp_adc_cal_raw_to_voltage(rawValue, &Config::adc_chars);
